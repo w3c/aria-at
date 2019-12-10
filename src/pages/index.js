6b44c8a0f1d48d983ebd70a12750e03296a6a95e
Valerie Young <spectranaut@gmail.com>
2019-12-10 13:43:34 -0800

    Fix url (#32)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ export default () => (
   <div style={{ textAlign: 'center' }}>
     <h2>ARIA-AT Test Runner Prototype</h2>
     <h3>
-    <Link to="/aria-at/runner">Run tests with the test runner</Link>
+    <Link to="/aria-at/runner/">Run tests with the test runner</Link>
     </h3>
     <h3>
-    <Link to="/aria-at/results">See results for previous test runs</Link>
+    <Link to="/aria-at/results/">See results for previous test runs</Link>
     </h3>
   </div>
 )

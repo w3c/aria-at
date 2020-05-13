826a942898006239a5501e342703c1a350c55754
Jon Gunderson <jongund@illinois.edu>
2020-05-13 13:28:13 -0500

    update stetup scripts

--- tests/menubar-editor/data/js/focusonfont.js
+++ tests/menubar-editor/data/js/focusonfont.js
@@ -1,4 +1,3 @@
 // Move focus to the "Font" menu item
-var node = testPageDocument.querySelectorAll('[role=menuitem]')[0];
-var menuId = testPageDocument.menubarEditor.getMenuId(node);
-testPageDocument.menubarEditor.setFocusToMenuitem(menuId, node);
+testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
+

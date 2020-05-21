33cc801f58a25046c4a4b17bea12156998c65f67
Jon Gunderson <jongund@illinois.edu>
2020-05-21 12:21:54 -0500

    updated tests

--- tests/menubar-editor/scripts.js
+++ tests/menubar-editor/scripts.js
@@ -15,6 +15,18 @@ var scripts = {
 	// Move focus to the "Font" menu item
 	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
 },
+	focusontextalign: function(testPageDocument){
+	// Move focus to the "Text Align" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
+},
+	focusontextalign: function(testPageDocument){
+	// Move focus to the "Text Align" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
+},
+	focusontextalign: function(testPageDocument){
+	// Move focus to the "Text Align" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
+},
 	focusonfont: function(testPageDocument){
 	// Move focus to the "Font" menu item
 	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
@@ -22,18 +34,6 @@ var scripts = {
 	focusonstylecolor: function(testPageDocument){
 	// Move focus to the "Style/Color" menu item
 	testPageDocument.querySelectorAll('[role=menuitem]')[1].focus();
-},
-	focusontextalign: function(testPageDocument){
-	// Move focus to the "Text Align" menu item
-	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
-},
-	focusontextalign: function(testPageDocument){
-	// Move focus to the "Text Align" menu item
-	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
-},
-	focusontextalign: function(testPageDocument){
-	// Move focus to the "Text Align" menu item
-	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
 },
 	focusonsize: function(testPageDocument){
 	// Move focus to the "Size" menu item

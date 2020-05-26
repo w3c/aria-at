6497c20dc2f16768c62e6b646602fcde9076c1e0
Valerie Young <spectranaut@gmail.com>
2020-05-26 14:56:15 -0700

    Regnerate tests (#198)

--- tests/menubar-editor/scripts.js
+++ tests/menubar-editor/scripts.js
@@ -7,6 +7,18 @@ var scripts = {
 	// Move focus to the link just before the meunbar
 	testPageDocument.querySelector('a').focus();
 },
+	focusonfirstlink: function(testPageDocument){
+	// Move focus to the link just before the meunbar
+	testPageDocument.querySelector('a').focus();
+},
+	focusonfirstlink: function(testPageDocument){
+	// Move focus to the link just before the meunbar
+	testPageDocument.querySelector('a').focus();
+},
+	focusonfont: function(testPageDocument){
+	// Move focus to the "Font" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
+},
 	focusonfont: function(testPageDocument){
 	// Move focus to the "Font" menu item
 	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
@@ -23,9 +35,37 @@ var scripts = {
 	// Move focus to the "Text Align" menu item
 	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
 },
+	focusontextalign: function(testPageDocument){
+	// Move focus to the "Text Align" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[2].focus();
+},
 	focusonfont: function(testPageDocument){
 	// Move focus to the "Font" menu item
 	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
+},
+	focusonfont: function(testPageDocument){
+	// Move focus to the "Font" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
+},
+	focusonfont: function(testPageDocument){
+	// Move focus to the "Font" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
+},
+	focusonfont: function(testPageDocument){
+	// Move focus to the "Font" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[0].focus();
+},
+	focusonstylecolor: function(testPageDocument){
+	// Move focus to the "Style/Color" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[1].focus();
+},
+	focusonstylecolor: function(testPageDocument){
+	// Move focus to the "Style/Color" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[1].focus();
+},
+	focusonstylecolor: function(testPageDocument){
+	// Move focus to the "Style/Color" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[1].focus();
 },
 	focusonstylecolor: function(testPageDocument){
 	// Move focus to the "Style/Color" menu item
@@ -38,6 +78,10 @@ var scripts = {
 	focusonsize: function(testPageDocument){
 	// Move focus to the "Size" menu item
 	testPageDocument.querySelectorAll('[role=menuitem]')[3].focus();
+},
+	focusonstylecolor: function(testPageDocument){
+	// Move focus to the "Style/Color" menu item
+	testPageDocument.querySelectorAll('[role=menuitem]')[1].focus();
 },
 	focusonstylecolor: function(testPageDocument){
 	// Move focus to the "Style/Color" menu item

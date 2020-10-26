823a5a23c5cc44ddcdacd182b237185d6ef126cd
Rebecca Meritz <rebecca@meritz.com>
2020-10-26 10:56:39 -0700

    Generate all the test via a github action
    - Add a new script for creating all tests instead of one example at a
    time
    - Remove all tests to demo adding the tests via a github action

---
+++ scripts/create-all-tests.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+const { createExampleTests } = require('./create-example-tests');
+
+const scriptDirectory = path.dirname(__filename);
+const rootDirectory = scriptDirectory.split('scripts')[0];
+
+const p = path.join(rootDirectory, 'tests');
+
+fs.readdirSync(p)
+  .filter(f => f !== 'resources' && fs.statSync(path.join(p, f)).isDirectory())
+  .forEach(d => createExampleTests(path.join('tests', d)));

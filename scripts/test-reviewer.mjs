e3377fb40ac5aceb8baca31c7e0f1595a243d328
Nick Schonning <nschonni@gmail.com>
2020-11-20 18:35:29 -0500

    chore: Add eslint-plugin-node

--- scripts/test-reviewer.mjs
+++ scripts/test-reviewer.mjs
@@ -1,44 +1,43 @@
 import path from 'path';
 import fs from 'fs';
 import fse from 'fs-extra';
-import htmlparser2 from 'htmlparser2';
 import { spawnSync } from 'child_process';
 import np from 'node-html-parser';
 import mustache from 'mustache';
 import { commandsAPI } from '../tests/resources/at-commands.mjs';
-import * as keys from '../tests/resources/keys.mjs';
 
 const testDir = path.resolve('.', 'tests');
 const templateFile = path.resolve('.', 'scripts', 'review-template.mustache');
-const templateIndexFile = path.resolve('.', 'scripts', 'review-index-template.mustache');
+const templateIndexFile = path.resolve(
+  '.',
+  'scripts',
+  'review-index-template.mustache'
+);
 const reviewDir = path.resolve('.', 'review');
 const allTestsForPattern = {};
-const support = JSON.parse(fse.readFileSync(path.join(testDir, 'support.json')));
+const support = JSON.parse(
+  fse.readFileSync(path.join(testDir, 'support.json'))
+);
 let allATKeys = [];
-support.ats.forEach(at => {
-	allATKeys.push(at.key);
+support.ats.forEach((at) => {
+  allATKeys.push(at.key);
 });
 const scripts = [];
 
-const getPriorityString = function(priority) {
+const getPriorityString = function (priority) {
   priority = parseInt(priority);
   if (priority === 1) {
     return 'required';
-  }
-  else if (priority === 2) {
+  } else if (priority === 2) {
     return 'optional';
   }
   return '';
-}
+};
 
 fse.readdirSync(testDir).forEach(function (subDir) {
   const subDirFullPath = path.join(testDir, subDir);
   const stat = fse.statSync(subDirFullPath);
-  if (
-    stat.isDirectory() &&
-    subDir !== 'resources'
-  ) {
-
+  if (stat.isDirectory() && subDir !== 'resources') {
     // Initialize the commands API
     const commandsJSONFile = path.join(subDirFullPath, 'commands.json');
     const commands = JSON.parse(fse.readFileSync(commandsJSONFile));
@@ -46,121 +45,157 @@ fse.readdirSync(testDir).forEach(function (subDir) {
 
     const tests = [];
 
-    const referencesCsv = fs.readFileSync(path.join(subDirFullPath, 'data', 'references.csv'), 'UTF-8');
-    const reference = referencesCsv.split(/\r?\n/).find(s => s.startsWith('reference,')).split(',')[1];
+    const referencesCsv = fs.readFileSync(
+      path.join(subDirFullPath, 'data', 'references.csv'),
+      'UTF-8'
+    );
+    const reference = referencesCsv
+      .split(/\r?\n/)
+      .find((s) => s.startsWith('reference,'))
+      .split(',')[1];
 
     const scriptsPath = path.join(subDirFullPath, 'data', 'js');
     fse.readdirSync(scriptsPath).forEach(function (scriptFile) {
       let script = '';
       try {
-        const data = fs.readFileSync(path.join(scriptsPath, scriptFile), 'UTF-8');
+        const data = fs.readFileSync(
+          path.join(scriptsPath, scriptFile),
+          'UTF-8'
+        );
         const lines = data.split(/\r?\n/);
         lines.forEach((line) => {
-          if (line.trim().length)
-            script += '\t' + line.trim() + '\n';
+          if (line.trim().length) script += '\t' + line.trim() + '\n';
         });
       } catch (err) {
         console.error(err);
       }
-      scripts.push(`\t${scriptFile.split('.js')[0]}: function(testPageDocument){\n${script}}`);
+      scripts.push(
+        `\t${
+          scriptFile.split('.js')[0]
+        }: function(testPageDocument){\n${script}}`
+      );
     });
 
     fse.readdirSync(subDirFullPath).forEach(function (test) {
-      if (path.extname(test) === '.html' && path.basename(test) !== 'index.html') {
+      if (
+        path.extname(test) === '.html' &&
+        path.basename(test) !== 'index.html'
+      ) {
+        const testFile = path.join(testDir, subDir, test);
+        const root = np.parse(fse.readFileSync(testFile, 'utf8'), {
+          script: true,
+        });
 
-	const testFile = path.join(testDir, subDir, test);
-	const root = np.parse(fse.readFileSync(testFile, 'utf8'), {script: true});
+        // Get metadata
+        const testFullName = root.querySelector('title').innerHTML;
+        const helpLinks = [];
+        for (let link of root.querySelectorAll('link')) {
+          if (link.attributes.rel === 'help') {
+            let href = link.attributes.href;
+            let text;
+            if (href.indexOf('#') >= 0) {
+              text = `ARIA specification: ${href.split('#')[1]}`;
+            } else {
+              text = `APG example: ${href.split('examples/')[1]}`;
+            }
 
-	// Get metadata
-	const testFullName = root.querySelector('title').innerHTML;
-	const helpLinks = [];
-	for (let link of root.querySelectorAll('link')) {
-	  if (link.attributes.rel === 'help') {
-	    let href = link.attributes.href;
-	    let text;
-	    if (href.indexOf('#') >= 0) {
-	      text = `ARIA specification: ${href.split('#')[1]}`;
-	    }
-	    else {
-	      text = `APG example: ${href.split('examples/')[1]}`;
-	    }
+            helpLinks.push({
+              link: href,
+              text: text,
+            });
+          }
+        }
 
-	    helpLinks.push({
-	      link: href,
-	      text: text
-	    });
-	  }
-	}
+        let testData = JSON.parse(
+          fse.readFileSync(
+            path.join(subDirFullPath, path.parse(test).name + '.json'),
+            'utf8'
+          )
+        );
 
-	let testData = JSON.parse(fse.readFileSync(path.join(subDirFullPath, path.parse(test).name+'.json'), 'utf8'));
+        const userInstruction = testData.specific_user_instruction;
+        const task = testData.task;
 
-	const userInstruction = testData.specific_user_instruction;
-	const task = testData.task;
+        // This is temporary while transitioning from lists to strings
+        const mode =
+          typeof testData.mode === 'string' ? testData.mode : testData.mode[0];
 
-	// This is temporary while transitioning from lists to strings
-	const mode = typeof testData.mode === 'string' ? testData.mode : testData.mode[0];
+        const ATTests = [];
 
-	const ATTests = [];
-
-	// TODO: These apply_to strings are not standarized yet.
-	let allReleventATs = [];
-	if (
-          testData.applies_to[0].toLowerCase() === "desktop screen readers"
-          || testData.applies_to[0].toLowerCase() === "screen readers"
+        // TODO: These apply_to strings are not standarized yet.
+        let allReleventATs = [];
+        if (
+          testData.applies_to[0].toLowerCase() === 'desktop screen readers' ||
+          testData.applies_to[0].toLowerCase() === 'screen readers'
         ) {
-	  allReleventATs = allATKeys;
-	}
-	else {
-	  allReleventATs = testData.applies_to;
-	}
+          allReleventATs = allATKeys;
+        } else {
+          allReleventATs = testData.applies_to;
+        }
 
-	for (const atKey of allReleventATs.map((a) => a.toLowerCase())) {
-	  let commands, assertions;
-	  let at = commAPI.isKnownAT(atKey);
+        for (const atKey of allReleventATs.map((a) => a.toLowerCase())) {
+          let commands, assertions;
+          let at = commAPI.isKnownAT(atKey);
 
-	  try {
-	    commands = commAPI.getATCommands(mode, task, at);
-	  }
-	  catch (error) {
-	  } // An error will occur if there is no data for a screen reader, ignore it
+          try {
+            commands = commAPI.getATCommands(mode, task, at);
+          } catch (error) {
+            // An error will occur if there is no data for a screen reader, ignore it
+          }
 
-	  if (testData.additional_assertions && testData.additional_assertions[at.key]) {
-	    assertions = testData.additional_assertions[at.key];
-	  }
-	  else {
-	    assertions = testData.output_assertions;
-	  }
+          if (
+            testData.additional_assertions &&
+            testData.additional_assertions[at.key]
+          ) {
+            assertions = testData.additional_assertions[at.key];
+          } else {
+            assertions = testData.output_assertions;
+          }
 
-	  ATTests.push({
-	    atName: at.name,
-		atKey: at.key,
-	    commands: commands && commands.length ? commands : undefined,
-	    assertions: assertions && assertions.length ? assertions.map(a => ({ priority: getPriorityString(a[0]), description: a[1] })) : undefined,
-	    userInstruction,
-	    modeInstruction: commAPI.getModeInstructions(mode, at),
-	    setupScriptDescription: testData.setup_script_description,
-	  });
-	}
+          ATTests.push({
+            atName: at.name,
+            atKey: at.key,
+            commands: commands && commands.length ? commands : undefined,
+            assertions:
+              assertions && assertions.length
+                ? assertions.map((a) => ({
+                    priority: getPriorityString(a[0]),
+                    description: a[1],
+                  }))
+                : undefined,
+            userInstruction,
+            modeInstruction: commAPI.getModeInstructions(mode, at),
+            setupScriptDescription: testData.setup_script_description,
+          });
+        }
 
-	// Create the test review pages
-	const testFilePath = path.join('.', 'tests', subDir, test);
-	const output = spawnSync('git', ['log', '-1', '--format="%ad"', testFilePath]);
-	const lastEdited = output.stdout.toString().replace(/"/gi, '').replace('\n', '');
+        // Create the test review pages
+        const testFilePath = path.join('.', 'tests', subDir, test);
+        const output = spawnSync('git', [
+          'log',
+          '-1',
+          '--format="%ad"',
+          testFilePath,
+        ]);
+        const lastEdited = output.stdout
+          .toString()
+          .replace(/"/gi, '')
+          .replace('\n', '');
 
-	tests.push({
-	  testNumber: tests.length+1,
-	  name: testFullName,
-	  location: `/${subDir}/${test}`,
+        tests.push({
+          testNumber: tests.length + 1,
+          name: testFullName,
+          location: `/${subDir}/${test}`,
           reference: `/${subDir}/${reference}`,
-	  allReleventATsFormatted: testData.applies_to.join(', '),
-	  allReleventATs: testData.applies_to,
+          allReleventATsFormatted: testData.applies_to.join(', '),
+          allReleventATs: testData.applies_to,
           setupScriptName: testData.setupTestPage,
-	  task,
-	  mode,
-	  ATTests,
-	  helpLinks,
-	  lastEdited
-	});
+          task,
+          mode,
+          ATTests,
+          helpLinks,
+          lastEdited,
+        });
       }
     });
 
@@ -171,22 +206,21 @@ fse.readdirSync(testDir).forEach(function (subDir) {
 });
 
 var template = fse.readFileSync(templateFile, 'utf8');
-if (!fse.existsSync(reviewDir)){
-    fse.mkdirSync(reviewDir);
+if (!fse.existsSync(reviewDir)) {
+  fse.mkdirSync(reviewDir);
 }
 
 var indexTemplate = fse.readFileSync(templateIndexFile, 'utf8');
 
-console.log("\n");
+console.log('\n');
 
 for (let pattern in allTestsForPattern) {
-
   var rendered = mustache.render(template, {
     pattern: pattern,
     totalTests: allTestsForPattern[pattern].length,
     tests: allTestsForPattern[pattern],
     AToptions: support.ats,
-    setupScripts: scripts
+    setupScripts: scripts,
   });
 
   let summaryFile = path.resolve(reviewDir, `${pattern}.html`);
@@ -195,18 +229,23 @@ for (let pattern in allTestsForPattern) {
 }
 
 const renderedIndex = mustache.render(indexTemplate, {
-  patterns: Object.keys(allTestsForPattern).map(pattern => {
-    const lastCommit = spawnSync('git', ['log', '-n1', '--oneline', path.join('.', 'tests', pattern)]).stdout.toString();
+  patterns: Object.keys(allTestsForPattern).map((pattern) => {
+    const lastCommit = spawnSync('git', [
+      'log',
+      '-n1',
+      '--oneline',
+      path.join('.', 'tests', pattern),
+    ]).stdout.toString();
     return {
       name: pattern,
       numberOfTests: allTestsForPattern[pattern].length,
       commit: lastCommit.split(' ')[0],
-      commitDescription: lastCommit
-    }
-  })
+      commitDescription: lastCommit,
+    };
+  }),
 });
 const indexFile = path.resolve('.', 'index.html');
 fse.writeFileSync(indexFile, renderedIndex);
 console.log(`Generated: ${indexFile}`);
 
-console.log("\n\nDone.");
+console.log('\n\nDone.');

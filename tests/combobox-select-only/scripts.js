8cae7ff532978091f32fa6f86c4aef73b8666a59
jscholes <jscholes@users.noreply.github.com>
2020-11-21 20:44:57 +0000

    Generate test and review files automatically

---
+++ tests/combobox-select-only/scripts.js
@@ -0,0 +1,186 @@
+var scripts = {
+	moveFocusBeforeCombobox: function(testPageDocument){
+	// sets focus on a link before the Combobox
+	testPageDocument.querySelector('#beforelink').focus();
+},
+	moveFocusAfterCombobox: function(testPageDocument){
+	// sets focus on a link after the Combobox
+	testPageDocument.querySelector('#afterlink').focus();
+},
+	moveFocusBeforeCombobox: function(testPageDocument){
+	// sets focus on a link before the Combobox
+	testPageDocument.querySelector('#beforelink').focus();
+},
+	moveFocusAfterCombobox: function(testPageDocument){
+	// sets focus on a link after the Combobox
+	testPageDocument.querySelector('#afterlink').focus();
+},
+	moveFocusBeforeCombobox: function(testPageDocument){
+	// sets focus on a link before the Combobox
+	testPageDocument.querySelector('#beforelink').focus();
+},
+	moveFocusAfterCombobox: function(testPageDocument){
+	// sets focus on a link after the Combobox
+	testPageDocument.querySelector('#afterlink').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	moveFocusToCombobox: function(testPageDocument){
+	// sets focus on the combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+},
+	openListboxToGuava: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Guave'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(11);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+},
+	openListboxToGuava: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Guave'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(11);
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListboxToHuckleberry: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Huckleberry'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(2);
+},
+	openListbox: function(testPageDocument){
+	// sets focus on and expands the Combobox
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+},
+	openListboxToHuckleberry: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Huckleberry'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(2);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+},
+	openListboxToApple: function(testPageDocument){
+	// sets focus on and expands the Combobox, and sets the focused option to 'Apple'
+	testPageDocument.querySelector('[role="combobox"]').focus();
+	window.selectController.updateMenuState(true);
+	window.selectController.onOptionChange(1);
+}
+};

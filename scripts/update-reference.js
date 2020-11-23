9451a001df2f4246645ae626e2869f0c2c07439d
Rebecca Meritz <rebecca@meritz.com>
2020-11-23 17:59:16 -0500

    Use example from reference.csv to locate example
    Instead of enforcing a 1-to-1 mapping between the apg-practices repo
    structure and this repo's structure use the already existing meta-data
    in references.csv to locate the correct example html file (and
    corresponding assests to pull in.)

--- scripts/update-reference.js
+++ scripts/update-reference.js
@@ -17,9 +17,9 @@ const args = require('minimist')(process.argv.slice(2), {
 if (args.help) {
   console.log(`
 Default use:
-  node update-reference.js directory
+  node update-reference.js exampleName
     Will copy the latest example html from aria-practices repo into this repo.
-    The directory name needs to match the name of an html file in the examples folder in the aria-practice repo.
+    The example html url for the example needs to be specificed in the references.csv file for the example named.
     The copied reference will be placed in a dated folder to provide a form of version control for the refernece files.
     The new reference will only be used with the reference.csv file is updated to point to the new reference folder.
   Arguments:
@@ -50,7 +50,14 @@ function locateFile(startPath, fileToFind) {
     }
 }
 
+
 function updateReferenceHtmlFile(referenceHtml) {
+  try {
+    fse.statSync(referenceHtml);
+  } catch (err) {
+    console.log("The reference example html '" + referenceHtml + "' does not exist. Please enure the provided example url in references.csv is correct.");
+    process.exit();
+  }
 	const root = np.parse(fse.readFileSync(referenceHtml, 'utf8'), {script: true});
   const head = root.querySelector('head');
   const header = root.querySelector('h1');
@@ -63,21 +70,51 @@ function updateReferenceHtmlFile(referenceHtml) {
 
 async function copyExampleToRepo(exampleName) {
   try {
+    const testDirectory = path.join('tests', exampleName);
+    try {
+      fse.statSync(testDirectory);
+    }
+    catch (err) {
+      console.log("The test directory '" + testDirectory + "' does not exist. Please enure the provide path name was correct.");
+      process.exit();
+    }
+
+    const referencesCsvFile = path.join(testDirectory, 'data', 'references.csv');
+    try {
+      fse.statSync(referencesCsvFile);
+    }
+    catch (err) {
+      console.log("The references.csv file does not exist. Please create '" + referencesCsvFile + "' file.");
+      process.exit();
+    }
+    const referencesCsv = fs.readFileSync(referencesCsvFile, 'UTF-8');
+    const exampleUrl = referencesCsv.split(/\r?\n/).find(s => s.startsWith('example,')).split(',')[1];
+    if (!exampleUrl) {
+      console.log('`example` must be defined in the references.csv file');
+      process.exit();
+    }
+    let examplePath;
+    if (exampleUrl.indexOf('https://w3c.github.io/aria-practices/examples/') >= 0) {
+      examplePath = exampleUrl.split('https://w3c.github.io/aria-practices/')[1];
+      examplePath = path.join(...examplePath.split('/')); // Ensure path type is correct regardless of OS
+      console.log(`Looking for ${examplePath} in the aria-practices repo`);
+    } else {
+      console.log('`example` must be defined in references.csv with the format `https://w3c.github.io/aria-practices/examples/<PATH_TO_EXAMPLE>.html`');
+      process.exit();
+    }
     await fse.remove(tmpPath);
     console.log('Cloning the aria-practice repo.');
     await git.Clone(cloneURL, tmpPath);
-    const examplesPath = path.join(tmpPath, 'examples')
-    const htmlFile = exampleName + '.html';
-    console.log(`Locating the matching example file ${htmlFile}`);
-    const htmlFileAbsolute = locateFile(examplesPath, htmlFile);
+    console.log(`Locating the matching example file ${examplePath}`);
+    const htmlFileAbsolute = path.join(tmpPath, examplePath);
     const currentDateTime = new Date();
     const formattedDateTime = currentDateTime.getFullYear() + "-" + (currentDateTime.getMonth() + 1) + "-" + currentDateTime.getDate() + "_" + currentDateTime.getHours() + + currentDateTime.getMinutes() + currentDateTime.getSeconds();
     const referenceDir = path.join(tmpPath, '..', '..', 'tests', exampleName, 'reference', formattedDateTime);
     await fse.ensureDir(referenceDir);
     const filterFunc = (src) => { return (src.indexOf('.html') == -1 || src == htmlFileAbsolute) };
     console.log('Coping assets to timestamped local directory.');
-    await fse.copy(path.join(examplesPath, htmlFileAbsolute.split('examples' + path.sep)[1].split(path.sep)[0]), referenceDir, { filter: filterFunc});
-    const referenceHtml = locateFile(referenceDir, htmlFile);
+    await fse.copy(path.join(tmpPath, 'examples', htmlFileAbsolute.split('examples' + path.sep)[1].split(path.sep)[0]), referenceDir, { filter: filterFunc});
+    const referenceHtml = locateFile(referenceDir, path.basename(examplePath));
     console.log(`Replacing reference html file with templated version\n\n`);
     updateReferenceHtmlFile(referenceHtml);
     const referenceHtmlPath = path.join('reference', referenceHtml.split('reference' + path.sep)[1]);

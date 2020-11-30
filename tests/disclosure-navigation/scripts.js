708cc0ea8f3262213547f4cf8d6bbbd2f11ea262
jscholes <jscholes@users.noreply.github.com>
2020-11-30 23:20:44 +0000

    Generate test and review files automatically

--- tests/disclosure-navigation/scripts.js
+++ tests/disclosure-navigation/scripts.js
@@ -31,7 +31,7 @@ var scripts = {
 	moveFocusToSecondDisclosureAndExpandFirstDisclosure: function(testPageDocument){
 	// sets focus on the second disclosure button in the navigation region, and sets the state of the first button to expanded without displaying its associated list of links
 	testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-	let firstMenu = document.querySelector('#id_about_menu');
+	let firstMenu = testPageDocument.querySelector('#id_about_menu');
 	testPageDocument.defaultView.disclosureController.toggleMenu(firstMenu, false);
 	testPageDocument.querySelector('button[aria-controls="id_admissions_menu"]').focus();
 },
@@ -43,7 +43,7 @@ var scripts = {
 	moveFocusToSecondDisclosureAndExpandFirstDisclosure: function(testPageDocument){
 	// sets focus on the second disclosure button in the navigation region, and sets the state of the first button to expanded without displaying its associated list of links
 	testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-	let firstMenu = document.querySelector('#id_about_menu');
+	let firstMenu = testPageDocument.querySelector('#id_about_menu');
 	testPageDocument.defaultView.disclosureController.toggleMenu(firstMenu, false);
 	testPageDocument.querySelector('button[aria-controls="id_admissions_menu"]').focus();
 },
@@ -55,7 +55,7 @@ var scripts = {
 	moveFocusToSecondDisclosureAndExpandFirstDisclosure: function(testPageDocument){
 	// sets focus on the second disclosure button in the navigation region, and sets the state of the first button to expanded without displaying its associated list of links
 	testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-	let firstMenu = document.querySelector('#id_about_menu');
+	let firstMenu = testPageDocument.querySelector('#id_about_menu');
 	testPageDocument.defaultView.disclosureController.toggleMenu(firstMenu, false);
 	testPageDocument.querySelector('button[aria-controls="id_admissions_menu"]').focus();
 },

ba9e94e4fcf18346c6c679efbafc17a404532dbb
James Scholes <james@jamesscholes.com>
2020-11-30 17:19:42 -0600

    Correct document reference in setup script

--- tests/disclosure-navigation/data/js/moveFocusToSecondDisclosureAndExpandFirstDisclosure.js
+++ tests/disclosure-navigation/data/js/moveFocusToSecondDisclosureAndExpandFirstDisclosure.js
@@ -1,5 +1,5 @@
 // sets focus on the second disclosure button in the navigation region, and sets the state of the first button to expanded without displaying its associated list of links
 testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-let firstMenu = document.querySelector('#id_about_menu');
+let firstMenu = testPageDocument.querySelector('#id_about_menu');
 testPageDocument.defaultView.disclosureController.toggleMenu(firstMenu, false);
 testPageDocument.querySelector('button[aria-controls="id_admissions_menu"]').focus();

4f3637863ee4e5cc9fb3cfc9d014ce1470ac3241
James Scholes <james@jamesscholes.com>
2020-11-30 17:31:25 -0600

    Update setup scripts to focus the first link in a dropdown rather than the button for the next menu

--- tests/disclosure-navigation/data/js/moveFocusToSecondDisclosureAndExpandFirstDisclosure.js
+++
@@ -1,5 +0,0 @@
-// sets focus on the second disclosure button in the navigation region, and sets the state of the first button to expanded without displaying its associated list of links
-testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-let firstMenu = testPageDocument.querySelector('#id_about_menu');
-firstMenu.style.display = 'none';
-testPageDocument.querySelector('button[aria-controls="id_admissions_menu"]').focus();

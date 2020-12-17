02c2fb0697d1055bbf4d1041fd78d45a6a71e364
James Scholes <james@jamesscholes.com>
2020-12-16 18:34:18 -0600

    Add setup scripts

---
+++ tests/combobox-autocomplete-both-updated/data/scripts/moveFocusAfterComboboxAndSetValueToAlabama.js
@@ -0,0 +1,3 @@
+// sets focus on a link after the Combobox, and sets the combobox value to 'Alabama'
+testPageDocument.defaultView.comboboxController.setValue('Alabama');
+testPageDocument.querySelector('#afterlink').focus();

c5536d0de78ee27203178752a2a8b060d929c9ae
James Scholes <james@jamesscholes.com>
2021-03-08 19:09:04 -0600

    Remove timeout

--- tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
+++ tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
@@ -1,6 +1,9 @@
 // sets focus on the first cell of the second row in the grid
+/*
 testPageDocument.defaultView.setTimeout(() => {
 	testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);
 	testPageDocument.defaultView.ex1Grid.focusCell(0, 0);
 }, 1000);
-// testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);
+*/
+testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);
+testPageDocument.defaultView.ex1Grid.focusCell(0, 0);

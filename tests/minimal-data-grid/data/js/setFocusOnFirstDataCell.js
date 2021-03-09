82fe4a85f10bac2295051c92f327f94b90e53d0d
James Scholes <james@jamesscholes.com>
2021-03-08 19:03:20 -0600

    Try setting timeout on different scope

--- tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
+++ tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
@@ -1,5 +1,5 @@
 // sets focus on the first cell of the second row in the grid
-window.setTimeout(() => {
+testPageDocument.defaultView.setTimeout(() => {
 	testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);
 }, 1000);
-testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);
+// testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);

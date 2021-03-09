b26180d96478ccc10a41f9bfb81b22fd47158743
James Scholes <james@jamesscholes.com>
2021-03-08 18:42:27 -0600

    Change cell coordinates in setup script

--- tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
+++ tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
@@ -1,2 +1,2 @@
 // sets focus on the first cell of the second row in the grid
-testPageDocument.defaultView.ex1Grid.setFocusPointer(1, 1);
+testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);

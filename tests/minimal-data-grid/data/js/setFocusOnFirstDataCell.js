875f80d98ebdf929e53a54372a02fef4283f8f2c
James Scholes <james@jamesscholes.com>
2021-03-08 19:00:43 -0600

    Try adding a timeout to setup scripts

--- tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
+++ tests/minimal-data-grid/data/js/setFocusOnFirstDataCell.js
@@ -1,2 +1,5 @@
 // sets focus on the first cell of the second row in the grid
+window.setTimeout(() => {
+	testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);
+}, 1000);
 testPageDocument.defaultView.ex1Grid.setFocusPointer(0, 0);

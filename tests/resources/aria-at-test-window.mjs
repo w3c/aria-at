753c7ae5393d4146ca0322ee29d6c4a7dd137d25
Michael "Z" Goddard <mzgoddard@gmail.com>
2021-09-15 11:10:38 -0400

    insert setup button reference page on DOMContentLoaded
    - remove reference page reload support

--- tests/resources/aria-at-test-window.mjs
+++ tests/resources/aria-at-test-window.mjs
@@ -28,46 +28,50 @@ export class TestWindow {
     this.scripts = scripts;
   }
 
-  /** If the window is closed, re-enable open popup button. */
   windowOnBeforeUnload() {
     window.setTimeout(() => {
       if (this.window.closed) {
         this.window = undefined;
-
-        this.hooks.windowClosed();
-      } else {
-        // If the window is open (after a location.reload()) rerun the setupTestPage script.
-        this.prepare();
       }
+
+      /** Re-enable open popup button. If the window was reloaded it may be 100ms  */
+      this.hooks.windowClosed();
     }, 100);
   }
 
+  windowOnDOMContentLoaded() {
+    if (this.window) {
+      this.prepare();
+    }
+  }
+
+  /**
+   * @param {Window} testPageWindow
+   */
   injectSetupResetButton(testPageWindow) {
+    const {scripts, setupScriptName} = this;
+    const setupScriptPresent = Boolean(setupScriptName && scripts[setupScriptName]);
+
+    /** @type {'setup' | 'post-setup'} */
+    let runSetupOrRelad = setupScriptPresent ? 'setup' : 'post-setup';
+
     const buttonDiv = testPageWindow.document.createElement('div');
     buttonDiv.innerHTML = `
       <div style="position: relative; left: 0; right: 0; height: 2rem;">
-        <button style="height: 100%; width: 100%;">Run Test Setup</button>
+        <button autofocus style="height: 100%; width: 100%;"${setupScriptPresent ? '' : ' disabled'}>Run Test Setup</button>
       </div>
     `;
     const buttonButton = buttonDiv.querySelector('button');
 
-    /** @type {'setup' | 'reload'} */
-    let runSetupOrRelad = 'setup';
-
-    const setupScriptName = this.setupScriptName;
-
     buttonButton.onclick = function() {
       try {
         if (runSetupOrRelad === 'setup') {
-          runSetupOrRelad = 'reload';
-          buttonButton.innerText = 'Reload Test';
+          runSetupOrRelad = 'post-setup';
+          buttonButton.disabled = true;
 
           if (setupScriptName) {
-            scripts[behavior.setupScriptName](testPageWindow.document);
+            scripts[setupScriptName](testPageWindow.document);
           }
-        } else {
-          testPageWindow.location.reload();
-          buttonButton.disabled = true;
         }
       } catch (error) {
         window.console.error(error);
@@ -75,32 +79,39 @@ export class TestWindow {
       }
     };
 
-    testPageWindow.document.body.insertBefore(buttonDiv.children[0], testPageWindow.document.body.children[0]);
+    const bodyElement = testPageWindow.document.body;
+    const mainElement = bodyElement.getElementsByTagName('main')[0] || bodyElement;
+
+    mainElement.appendChild(buttonDiv.children[0]);
   }
 
   open() {
+    if (this.window) {
+      this.window.close();
+    }
+
     this.window = window.open(this.pageUri, "_blank", "toolbar=0,location=0,menubar=0,width=400,height=400");
 
     this.hooks.windowOpened();
 
-    // If the window is closed, re-enable open popup button
-    this.window.onbeforeunload = this.windowOnBeforeUnload.bind(this);
+    // Prepare the window once we can modify it.
+    this.window.addEventListener('DOMContentLoaded', this.windowOnDOMContentLoaded.bind(this));
   }
 
-  prepare() {
+  async prepare() {
     if (!this.window) {
       return;
     }
 
-    // make sure the origin is the same, and prevent this from firing on an 'about' page
-    if (
+    // Re-enable open button when unloaded by closing the window or reloading it.
+    this.window.onbeforeunload = this.windowOnBeforeUnload.bind(this);
+
+    // Make sure the origin is the same, and prevent this from firing on an 'about' page.
+    while (
       this.window.location.origin !== window.location.origin ||
-      this.window.document.readyState !== "complete"
+      this.window.document.readyState !== "complete" && this.window.document.readyState !== "interactive"
     ) {
-      window.setTimeout(() => {
-        this.prepare();
-      }, 100);
-      return;
+      await new Promise(resolve => window.setTimeout(resolve, 100));
     }
 
     this.injectSetupResetButton(this.window);

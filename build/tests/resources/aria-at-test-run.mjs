38e656539b544c1a9e8c14239deb5ff9e121e5df
mzgoddard <mzgoddard@users.noreply.github.com>
2021-09-21 15:03:23 +0000

    Generate test and review files automatically

--- build/tests/resources/aria-at-test-run.mjs
+++ build/tests/resources/aria-at-test-run.mjs
@@ -118,9 +118,9 @@ export function instructionDocument(resultState, hooks) {
 
   return {
     errors: {
-      visible: false,
+      visible: resultState.errors && resultState.errors.length > 0 ? true : false,
       header: "Test cannot be performed due to error(s)!",
-      errors: [],
+      errors: resultState.errors || [],
     },
     instructions: {
       header: {
@@ -681,9 +681,10 @@ function testPageDocument(state, hooks) {
       results: resultsTableDocument(state),
     };
   }
+  const instructions = instructionDocument(state, hooks);
   return {
-    errors: null,
-    instructions: instructionDocument(state, hooks),
+    errors: instructions.errors,
+    instructions,
   };
 }
 

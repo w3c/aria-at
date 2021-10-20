acc6cb130ab5a6cbf23cbb70c76e7fffd876734f
James Scholes <james@jamesscholes.com>
2021-10-20 13:32:55 -0500

    Toggle Button: fix setup scripts to set state of button to 'pressed' in some cases which were previously not accounted for (#532)
    * Toggle Button: fix setup scripts to set state of button to 'pressed' in some cases which were previously not accounted for
    
    * Generate test and review files automatically
    
    Co-authored-by: jscholes <jscholes@users.noreply.github.com>

---
+++ build/tests/toggle-button/scripts/setFocusAfterButtonAndSetStateToPressed.jsonp.js
@@ -0,0 +1,9 @@
+window[document.currentScript.getAttribute("jsonpFunction") || "scriptsJsonpLoaded"]({
+  setFocusAfterButtonAndSetStateToPressed(testPageDocument) {
+    // sets focus on a link after the button, and sets the state of the button to 'pressed'
+    let button = testPageDocument.querySelector('#toggle');
+    button.setAttribute('aria-pressed', 'true');
+    button.querySelector('use').setAttribute('xlink:href', '#icon-sound');
+    testPageDocument.querySelector('#afterlink').focus();
+  }
+});

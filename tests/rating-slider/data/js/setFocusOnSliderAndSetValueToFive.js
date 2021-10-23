dc1413f271fed287de7e1be7cbb41630d0f0ebf9
James Scholes <james@jamesscholes.com>
2021-10-23 13:53:00 -0500

    Update setup scripts to include maximum value in aria-valuetext

--- tests/rating-slider/data/js/setFocusOnSliderAndSetValueToFive.js
+++ tests/rating-slider/data/js/setFocusOnSliderAndSetValueToFive.js
@@ -1,5 +1,5 @@
 // sets focus on the slider, and sets its value to five stars
 let slider = testPageDocument.querySelector('[role="slider"]');
 slider.setAttribute('aria-valuenow', '5');
-slider.setAttribute('aria-valuetext', 'five stars');
+slider.setAttribute('aria-valuetext', 'five of five stars');
 slider.focus();

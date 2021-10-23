fe266d9e34c4fbf640d05e1f4c491ff5d712dc3e
IsaDC <70024509+IsaDC@users.noreply.github.com>
2021-10-23 13:56:15 -0500

    Create tests for APG design pattern example: Rating Slider (#486)
    * Add references.csv
    
    * Add timestamped example directory and update references.csv
    
    * Add commands.csv
    
    * Add tests.csv
    
    * Remove extra quotes from command sequences
    
    * Example: remove unneeded CSS/JS/images and update page title
    
    * Example: remove 'Related Links' nav and similar example links, as well as 'Accessibility Features', 'Keyboard Support', 'Role, Property, State, and Tabindex Attributes' and source code sections and footer nav
    
    * Example: add links before and after the slider for use in setup scripts
    
    More details: https://github.com/w3c/aria-at/commit/4357a2e0613e338671d9207f08cdd393cf595a4f
    
    * Add setup scripts
    
    * Generate test and review files automatically
    
    * Update setup scripts to include maximum value in aria-valuetext
    
    * Generate test and review files automatically
    
    Co-authored-by: James Scholes <james@jamesscholes.com>
    Co-authored-by: jscholes <jscholes@users.noreply.github.com>

---
+++ build/tests/rating-slider/scripts/setFocusOnSliderAndSetValueToOneHalfStar.module.js
@@ -0,0 +1,7 @@
+export function setFocusOnSliderAndSetValueToOneHalfStar(testPageDocument) {
+  // sets focus on the slider, and sets its value to one half star
+  let slider = testPageDocument.querySelector('[role="slider"]');
+  slider.setAttribute('aria-valuenow', '0.5');
+  slider.setAttribute('aria-valuetext', 'one half of five stars');
+  slider.focus();
+}

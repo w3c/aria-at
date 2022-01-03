13b2d4a4a4c096ccc402dc0d29187a7c8ebc3a2d
jscholes <jscholes@users.noreply.github.com>
2022-01-03 16:24:33 +0000

    Generate test and review files automatically

--- build/tests/datepicker-spin-button/scripts.module.js
+++ build/tests/datepicker-spin-button/scripts.module.js
@@ -2,8 +2,8 @@ export function setFocusAfterSpinButtonAndSetPredefinedDate(testPageDocument) {
   // sets the date picker to March 19, 2021, and sets focus on a link after the 'Year' spin button
   testPageDocument.defaultView.datepicker.spinbuttonYear.setValue(2021);
   testPageDocument.defaultView.datepicker.spinbuttonMonth.setValue(2);
-  testPageDocument.defaultView.datepicker.spinbuttonYear.setValue(19);
-  testPageDocument.querySelectorAll('button').forEach(btn => btn.style.display = 'none');
+  testPageDocument.defaultView.datepicker.spinbuttonDay.setValue(19);
+  testPageDocument.querySelectorAll('button').forEach(btn => (btn.style.display = 'none'));
   testPageDocument.querySelector('#afterlink').focus();
 }
 
@@ -11,8 +11,8 @@ export function setFocusBeforeSpinButtonAndSetPredefinedDate(testPageDocument) {
   // sets the date picker to March 19, 2021, and sets focus on a link before the 'Day' spin button
   testPageDocument.defaultView.datepicker.spinbuttonYear.setValue(2021);
   testPageDocument.defaultView.datepicker.spinbuttonMonth.setValue(2);
-  testPageDocument.defaultView.datepicker.spinbuttonYear.setValue(19);
-  testPageDocument.querySelectorAll('button').forEach(btn => btn.style.display = 'none');
+  testPageDocument.defaultView.datepicker.spinbuttonDay.setValue(19);
+  testPageDocument.querySelectorAll('button').forEach(btn => (btn.style.display = 'none'));
   testPageDocument.querySelector('#beforelink').focus();
 }
 
@@ -20,7 +20,7 @@ export function setFocusOnSpinButtonAndSetPredefinedDate(testPageDocument) {
   // sets the date picker to March 19, 2021, and sets focus on the 'Day' spin button
   testPageDocument.defaultView.datepicker.spinbuttonYear.setValue(2021);
   testPageDocument.defaultView.datepicker.spinbuttonMonth.setValue(2);
-  testPageDocument.defaultView.datepicker.spinbuttonYear.setValue(19);
-  testPageDocument.querySelectorAll('button').forEach(btn => btn.style.display = 'none');
+  testPageDocument.defaultView.datepicker.spinbuttonDay.setValue(19);
+  testPageDocument.querySelectorAll('button').forEach(btn => (btn.style.display = 'none'));
   testPageDocument.defaultView.datepicker.spinbuttonDay.spinbuttonNode.focus();
 }

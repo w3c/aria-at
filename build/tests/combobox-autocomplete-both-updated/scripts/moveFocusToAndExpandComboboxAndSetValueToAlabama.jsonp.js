a906787e037a0faca5b9ea25868b3e4f2b6ba458
James Scholes <james@jamesscholes.com>
2022-01-04 13:31:36 -0600

    Remove build directory

--- build/tests/combobox-autocomplete-both-updated/scripts/moveFocusToAndExpandComboboxAndSetValueToAlabama.jsonp.js
+++
@@ -1,9 +0,0 @@
-window[document.currentScript.getAttribute("jsonpFunction") || "scriptsJsonpLoaded"]({
-  moveFocusToAndExpandComboboxAndSetValueToAlabama(testPageDocument) {
-    // sets focus on and expands the combobox, and sets the combobox value to 'Alabama'
-    testPageDocument.defaultView.comboboxController.setValue('Alabama');
-    testPageDocument.defaultView.comboboxController.open();
-    testPageDocument.defaultView.comboboxController.comboboxNode.select();
-    testPageDocument.defaultView.comboboxController.comboboxNode.focus();
-  }
-});
--- build/tests/disclosure-navigation/scripts/moveFocusToFirstLinkInDropdown.jsonp.js
+++
@@ -1,7 +0,0 @@
-window[document.currentScript.getAttribute("jsonpFunction") || "scriptsJsonpLoaded"]({
-  moveFocusToFirstLinkInDropdown(testPageDocument) {
-    // sets the state of the first disclosure button to expanded, and sets focus on the first link of the associated dropdown
-    testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-    testPageDocument.querySelector('#id_about_menu a').focus();
-  }
-});

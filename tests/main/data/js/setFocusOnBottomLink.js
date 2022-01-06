2f3b59b039db5b734e92ce8ae229708ab138709b
James Scholes <james@jamesscholes.com>
2022-01-06 14:45:52 -0600

    Hide setup button as needed

--- tests/main/data/js/setFocusOnBottomLink.js
+++ tests/main/data/js/setFocusOnBottomLink.js
@@ -1,2 +1,3 @@
 // sets focus on the 'Bottom' link
+testPageDocument.querySelector('.button-run-test-setup').parentElement.style.display = 'none';
 testPageDocument.getElementById('bottom').focus();

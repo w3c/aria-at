9c01dbbecfd93389ddf62910b2366a8360ac074f
jscholes <jscholes@users.noreply.github.com>
2022-01-06 19:55:20 +0000

    Generate test and review files automatically

--- build/tests/form/scripts/setFocusAfterFormAndHideBottomControls.module.js
+++ build/tests/form/scripts/setFocusAfterFormAndHideBottomControls.module.js
@@ -1,6 +1,6 @@
 export function setFocusAfterFormAndHideBottomControls(testPageDocument) {
   // sets focus on a link after the form landmark, and hides the 'Bottom' link and 'Add Contact' button
-  testPageDocument.querySelector('[type="submit"]').setAttribute('hidden', '');
+  testPageDocument.querySelector('[type="submit"]').style.display = 'none';
   testPageDocument.getElementById('bottom').setAttribute('hidden', '');
   testPageDocument.getElementById('afterlink').focus();
 }

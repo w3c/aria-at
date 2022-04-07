de14b22c578e5baba590ce8e99e4e1689f4d18e3
James Scholes <james@jamesscholes.com>
2022-04-07 16:05:58 -0500

    Add new setup scripts

--- tests/radiogroup-aria-activedescendant/data/js/setFocusOnFirstRadioButtonAndCheckSecondRadioButton.js
+++ tests/radiogroup-aria-activedescendant/data/js/setFocusOnFirstRadioButtonAndCheckSecondRadioButton.js
@@ -1,1 +1,11 @@
 // sets focus on the first radio button, and checks the  second radio button
+let radioGroup = testPageDocument.querySelector('[role="radiogroup"]');
+let radios = testPageDocument.querySelectorAll('[role="radio"]');
+radios.forEach(r => {
+  r.setAttribute('aria-checked', 'false');
+  r.classList.remove('focus');
+});
+radios[0].classList.add('focus');
+radios[1].setAttribute('aria-checked', 'true');
+radioGroup.setAttribute('aria-activedescendant', radios[0].id);
+radioGroup.focus();

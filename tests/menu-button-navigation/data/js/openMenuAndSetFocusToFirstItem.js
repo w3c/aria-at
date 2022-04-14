1fde762fb9d5d71123fadae35c139353d9bc65db
Isabel Del Castillo <isa.delcastillo5@gmail.com>
2022-04-14 15:47:30 -0500

    setupScript: add set focus on first and last menuitem.

--- tests/menu-button-navigation/data/js/openMenuAndSetFocusToFirstItem.js
+++ tests/menu-button-navigation/data/js/openMenuAndSetFocusToFirstItem.js
@@ -1,1 +1,3 @@
 // opens the menu, and sets focus on 'W3C Home Page'
+testPageDocument.defaultView.menuController.openPopup();
+testPageDocument.defaultView.menuController.setFocusToFirstMenuitem();

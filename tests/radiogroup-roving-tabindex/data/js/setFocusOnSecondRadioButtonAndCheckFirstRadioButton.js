76547989b2a9768df00faa9c8bded8d9ddb58b20
IsaDC <70024509+IsaDC@users.noreply.github.com>
2023-03-09 15:02:59 -0600

    Additional tests for Radio Group Example Using Roving tabindex - amended (#906)
    * Add separated tests for navigating to next and previous checked and unchecked radio buttons
    
    * Update tests and commands to latest test plan version
    
    * Delete unnecesary reference and add missing setupScript
    
    * Generate .html source files with scripts automatically
    
    ---------
    
    Co-authored-by: IsaDC <IsaDC@users.noreply.github.com>

--- tests/radiogroup-roving-tabindex/data/js/setFocusOnSecondRadioButtonAndCheckFirstRadioButton.js
+++ tests/radiogroup-roving-tabindex/data/js/setFocusOnSecondRadioButtonAndCheckFirstRadioButton.js
@@ -1,11 +1,11 @@
-// sets focus on the first radio button, and sets its state to checked
+// sets focus on the second radio button, and checks the first radio button
 let radioGroup = testPageDocument.querySelector('[role="radiogroup"]');
 let radios = testPageDocument.querySelectorAll('[role="radio"]');
 radios.forEach(r => {
   r.setAttribute('aria-checked', 'false');
   r.classList.remove('focus');
 });
-radios[0].classList.add('focus');
+radios[1].classList.add('focus');
 radios[0].setAttribute('aria-checked', 'true');
-radioGroup.setAttribute('aria-activedescendant', radios[0].id);
+radioGroup.setAttribute('aria-activedescendant', radios[1].id);
 radioGroup.focus();
--- tests/radiogroup-roving-tabindex/data/js/setFocusOnThirdRadioButton.js
+++ tests/radiogroup-roving-tabindex/data/js/setFocusOnThirdRadioButton.js
@@ -1,9 +1,10 @@
 // sets focus on the third radio button
+let radioGroup = testPageDocument.querySelector('[role="radiogroup"]');
 let radios = testPageDocument.querySelectorAll('[role="radio"]');
 radios.forEach(r => {
   r.setAttribute('aria-checked', 'false');
-  r.tabIndex = -1;
+  r.classList.remove('focus');
 });
-let lastRadio = radios[radios.length - 1];
-lastRadio.tabIndex = 0;
-lastRadio.focus();
+radios[2].classList.add('focus');
+radioGroup.setAttribute('aria-activedescendant', radios[2].id);
+radioGroup.focus();

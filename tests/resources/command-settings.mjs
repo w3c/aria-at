3ce9b66e3e6896c2581d88dfe270f2744d4e10df
Matt King <a11yThinker@gmail.com>
2023-08-07 00:19:22 -0700

    finish refactor of slider test plan to V2 of format

---
+++ tests/resources/command-settings.mjs
@@ -0,0 +1,10 @@
+// command settings: settings that must be active before a command is executed.
+// Referenced by commands specified in AT_KEY-commands.csv
+export const JAWS_VIRTUAL_CURSOR = "virtual cursor active";
+export const JAWS_PC_CURSOR = "PC cursor active";
+export const NVDA_BROWSE_MODE = "browse mode on";
+export const NVDA_FOCUS_MODE= "focus mode on";
+export const VOICEOVER_QUICKNAV_ON = "quick nav on";
+export const VOICEOVER_QUICKNAV_OFF = "quick nav off";
+export const NARRATOR_SCANMODE_ON = "scan mode on";
+export const NARRATOR_SCANMODE_OFF = "scan mode off";

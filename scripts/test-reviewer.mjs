494632fa2e146959783978fac8e11f031b71dde1
Howard Edwards <howarde.edwards@gmail.com>
2023-10-09 21:29:38 -0400

    Formatting

--- scripts/test-reviewer.mjs
+++ scripts/test-reviewer.mjs
@@ -592,29 +592,31 @@ if (TARGET_TEST_PLAN) {
 }
 
 const renderedIndex = mustache.render(indexTemplate, {
-  patterns: Object.keys(allTestsForPattern).map(pattern => {
-    // TODO: useful for determining smart-diffs; this has to continue generating for all patterns until smart-diffs come into play
-    const lastCommit = spawnSync('git', [
-      'log',
-      '-n1',
-      '--oneline',
-      path.join('.', 'tests', pattern),
-    ]).stdout.toString();
-    return {
-      patternName: pattern,
-      title: allTestsForPattern[pattern][0].title,
-      numberOfTests: allTestsForPattern[pattern].length,
-      commit: lastCommit.split(' ')[0],
-      commitDescription: lastCommit,
-    };
-  }).sort((a, b) => {
-    const titleA = a.title.toUpperCase();
-    const titleB = b.title.toUpperCase();
+  patterns: Object.keys(allTestsForPattern)
+    .map(pattern => {
+      // TODO: useful for determining smart-diffs; this has to continue generating for all patterns until smart-diffs come into play
+      const lastCommit = spawnSync('git', [
+        'log',
+        '-n1',
+        '--oneline',
+        path.join('.', 'tests', pattern),
+      ]).stdout.toString();
+      return {
+        patternName: pattern,
+        title: allTestsForPattern[pattern][0].title,
+        numberOfTests: allTestsForPattern[pattern].length,
+        commit: lastCommit.split(' ')[0],
+        commitDescription: lastCommit,
+      };
+    })
+    .sort((a, b) => {
+      const titleA = a.title.toUpperCase();
+      const titleB = b.title.toUpperCase();
 
-    if (titleA < titleB) return -1;
-    if (titleA > titleB) return 1;
-    return 0;
-  }),
+      if (titleA < titleB) return -1;
+      if (titleA > titleB) return 1;
+      return 0;
+    }),
 });
 
 fse.writeFileSync(indexFileBuildOutputPath, renderedIndex);

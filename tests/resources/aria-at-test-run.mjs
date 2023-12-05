87bbf82c44d264cc17d0ad1ffde15469668d0082
Howard Edwards <howarde.edwards@gmail.com>
2023-12-05 14:26:50 -0500

    Update aria-at-test-run and aria-at-test-io-format to support 0-level assertionExceptions

--- tests/resources/aria-at-test-run.mjs
+++ tests/resources/aria-at-test-run.mjs
@@ -219,7 +219,7 @@ export function instructionDocument(resultState, hooks) {
     const resultUnexpectedBehavior = resultStateCommand.unexpected;
 
     const {
-      commandSettings: { description: settings, text: settingsText },
+      commandSettings: { description: settings, text: settingsText, assertionExceptions },
     } = resultStateCommand;
 
     return {
@@ -248,7 +248,15 @@ export function instructionDocument(resultState, hooks) {
         }?`,
       },
       assertions: [
-        ...assertions.map(bind(assertionResult, commandIndex)),
+        ...assertions
+          .filter((each, index) => (assertionExceptions ? assertionExceptions[index] !== 0 : each))
+          .map(each =>
+            assertionResult(
+              commandIndex,
+              each,
+              assertions.findIndex(e => e === each)
+            )
+          ),
         ...additionalAssertions.map(bind(additionalAssertionResult, commandIndex)),
       ],
       unexpectedBehaviors: {
@@ -742,10 +750,19 @@ function resultsTableDocument(state) {
       header: [
         'Test result: ',
         state.commands.some(
-          ({ assertions, additionalAssertions, unexpected }) =>
-            [...assertions, ...additionalAssertions].some(
-              ({ priority, result }) => priority === 1 && result !== CommonResultMap.PASS
-            ) || unexpected.behaviors.some(({ checked }) => checked)
+          ({
+            assertions,
+            additionalAssertions,
+            unexpected,
+            commandSettings: { assertionExceptions },
+          }) =>
+            [
+              ...assertions.filter((each, index) =>
+                assertionExceptions ? assertionExceptions[index] !== 0 : each
+              ),
+              ...additionalAssertions,
+            ].some(({ priority, result }) => priority === 1 && result !== CommonResultMap.PASS) ||
+            unexpected.behaviors.some(({ checked }) => checked)
         )
           ? 'FAIL'
           : 'PASS',
@@ -758,11 +775,19 @@ function resultsTableDocument(state) {
         details: 'Details',
       },
       commands: state.commands.map(command => {
-        const allAssertions = [...command.assertions, ...command.additionalAssertions];
+        const {
+          commandSettings: { assertionExceptions },
+        } = command;
+        const allAssertions = [
+          ...command.assertions.filter((each, index) =>
+            assertionExceptions ? assertionExceptions[index] !== 0 : each
+          ),
+          ...command.additionalAssertions,
+        ];
 
         let passingAssertions = ['No passing assertions.'];
         let failingAssertions = ['No failing assertions.'];
-        let unexpectedBehaviors = ['No unexpect behaviors.'];
+        let unexpectedBehaviors = ['No unexpected behaviors.'];
 
         if (allAssertions.some(({ result }) => result === CommonResultMap.PASS)) {
           passingAssertions = allAssertions

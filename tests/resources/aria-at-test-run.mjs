008a14a208b70e8a5fa3e244d9e43a0c3c317a90
Mike Pennisi <mike@mikepennisi.com>
2025-04-29 16:18:06 -0400

    Add ability to designate tests as "untestable"

--- tests/resources/aria-at-test-run.mjs
+++ tests/resources/aria-at-test-run.mjs
@@ -22,6 +22,7 @@ export class TestRun {
       setCommandUnexpectedBehaviorImpact: bindDispatch(userChangeCommandUnexpectedBehaviorImpact),
       setCommandUnexpectedBehaviorMore: bindDispatch(userChangeCommandUnexpectedBehaviorMore),
       setCommandOutput: bindDispatch(userChangeCommandOutput),
+      setCommandUntestable: bindDispatch(userChangeCommandUntestable),
       submit: () => submitResult(this),
       ...hooks,
     };
@@ -252,6 +253,21 @@ export function instructionDocument(resultState, hooks) {
           focusFirstRequired(),
         change: atOutput => hooks.setCommandOutput({ commandIndex, atOutput }),
       },
+      untestable: {
+        description: [
+          `The response to '${command}' created conditions that made assertions untestable, e.g., focused the wrong element.`,
+          {
+            highlightRequired: resultStateCommand.untestable.highlightRequired,
+            description: '(requires at least one SEVERE undesirable behavior)',
+          },
+        ],
+        value: resultStateCommand.untestable.value,
+        focus:
+          resultState.currentUserAction === 'validateResults' &&
+          resultStateCommand.untestable.highlightRequired &&
+          focusFirstRequired(),
+        change: isUntestable => hooks.setCommandUntestable({ commandIndex, isUntestable })
+      },
       assertionsHeader: {
         descriptionHeader: `${resultState.testPlanStrings.assertionResponseQuestion} ${command}${
           settingsText && settings !== 'defaultMode' ? ` (${settingsText})` : ''
@@ -430,6 +446,7 @@ export const UserActionMap = createEnumMap({
   CLOSE_TEST_WINDOW: 'closeTestWindow',
   VALIDATE_RESULTS: 'validateResults',
   CHANGE_TEXT: 'changeText',
+  CHANGE_UNTESTABLE: 'changeUntestable',
   CHANGE_SELECTION: 'changeSelection',
   SHOW_RESULTS: 'showResults',
 });
@@ -509,6 +526,49 @@ export function userChangeCommandOutput({ commandIndex, atOutput }) {
               atOutput: {
                 ...commandState.atOutput,
                 value: atOutput,
+              },
+            }
+      ),
+    };
+  };
+}
+
+/**
+ * @param {object} props
+ * @param {number} props.commandIndex
+ * @param {boolean} props.isUntestable
+ * @returns {(state: TestRunState) => TestRunState}
+ */
+export function userChangeCommandUntestable({ commandIndex, isUntestable }) {
+  return function (state) {
+
+    // When the user indicates that a command is untestable, they are tacitly
+    // signalling that there was at least one unexpected behavior.
+    let newState = userChangeCommandHasUnexpectedBehavior({
+      commandIndex,
+      hasUnexpected: isUntestable ?
+        HasUnexpectedBehaviorMap.HAS_UNEXPECTED : HasUnexpectedBehaviorMap.NOT_SET
+    })(state);
+
+    const total = newState.commands[commandIndex].assertions.length
+    const result = isUntestable ? false : AssertionResultMap.NOT_SET;
+    for (let assertionIndex = 0; assertionIndex < total; ++assertionIndex) {
+      newState = userChangeCommandAssertion({
+        commandIndex, assertionIndex, result
+      })(newState);
+    }
+
+    return {
+      ...newState,
+      currentUserAction: UserActionMap.CHANGE_UNTESTABLE,
+      commands: newState.commands.map((commandState, index) =>
+        index !== commandIndex
+          ? commandState
+          : {
+              ...commandState,
+              untestable: {
+                ...commandState.untestable,
+                value: isUntestable,
               },
             }
       ),
@@ -761,6 +821,10 @@ function submitResult(app) {
     return;
   }
 
+  if (app.state.commands.some(needsSevereImpact)) {
+    return;
+  }
+
   app.hooks.postResults();
 
   app.hooks.closeTestPage();
@@ -793,6 +857,27 @@ function isSomeFieldRequired(state) {
           command.unexpected.behaviors.some(
             behavior => behavior.checked && behavior.more && behavior.more.value.trim() === ''
           )))
+  );
+}
+
+/**
+ * @param {TestRunCommand} command
+ * @returns {boolean}
+ */
+function needsSevereImpact(command) {
+  return !!command.untestable.value && !commandHasSevereImpact(command);
+}
+
+/**
+ * @param {TestRunCommand} command
+ * @returns {boolean}
+ */
+function commandHasSevereImpact(command) {
+  return (
+    command.unexpected.hasUnexpected === HasUnexpectedBehaviorMap.HAS_UNEXPECTED &&
+    command.unexpected.behaviors.some(
+      behavior => behavior.checked && behavior.impact === 'SEVERE'
+    )
   );
 }
 
@@ -982,6 +1067,10 @@ export function userValidateState() {
           atOutput: {
             ...command.atOutput,
             highlightRequired: !command.atOutput.value.trim(),
+          },
+          untestable: {
+            ...command.untestable,
+            highlightRequired: needsSevereImpact(command)
           },
           unexpected: {
             ...command.unexpected,
@@ -1213,6 +1302,7 @@ export function userValidateState() {
  * @property {(options: {commandIndex: number, assertionIndex: number, result: AssertionResult}) => void} setCommandAssertion
  * @property {(options: {commandIndex: number, hasUnexpected: HasUnexpectedBehavior}) => void } setCommandHasUnexpectedBehavior
  * @property {(options: {commandIndex: number, atOutput: string}) => void} setCommandOutput
+ * @property {(options: {commandIndex: number, isUntestable: boolean}) => void} setCommandUntestable
  * @property {(options: {commandIndex: number, unexpectedIndex: number, checked}) => void } setCommandUnexpectedBehavior
  * @property {(options: {commandIndex: number, unexpectedIndex: number, impact: string}) => void } setCommandUnexpectedBehaviorImpact
  * @property {(options: {commandIndex: number, unexpectedIndex: number, more: string}) => void } setCommandUnexpectedBehaviorMore

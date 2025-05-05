75ea6e84c1dbaf2a949883f19869a433b5428b0a
jscholes <6454755+jscholes@users.noreply.github.com>
2025-05-05 17:23:41 +0000

    Generate .html source files with scripts automatically

--- build/tests/disclosure-faq/scripts.module.js
+++ build/tests/disclosure-faq/scripts.module.js
@@ -1,14 +1,14 @@
-export function moveFocusAfterAndExpandFirstDisclosure(testPageDocument) {
-  // sets the state of the first disclosure button to expanded, and sets focus on a link after the button
-  const btn = testPageDocument.querySelector('button[aria-controls="faq1_desc"]');
-  const answer = testPageDocument.querySelector('#faq1_desc');
+export function moveFocusAfterAndExpandFourthDisclosure(testPageDocument) {
+  // sets the state of the fourth disclosure button to expanded, and sets focus on a link after the button
+  const btn = testPageDocument.querySelector('button[aria-controls="faq4_desc"]');
+  const answer = testPageDocument.querySelector('#faq4_desc');
   btn.setAttribute('aria-expanded', 'true');
   answer.style.display = 'block';
   testPageDocument.querySelector('#afterlink').focus();
 }
 
-export function moveFocusAfterFirstDisclosure(testPageDocument) {
-  // sets focus on a link after the first disclosure button
+export function moveFocusAfterFourthDisclosure(testPageDocument) {
+  // sets focus on a link after the fourth disclosure button
   testPageDocument.querySelector('#afterlink').focus();
 }
 
@@ -37,7 +37,23 @@ export function moveFocusToAndExpandFirstDisclosure(testPageDocument) {
   btn.focus();
 }
 
+export function moveFocusToAndExpandFourthDisclosure(testPageDocument) {
+  // sets focus on the fourth disclosure button, and sets its state to expanded
+  const btn = testPageDocument.querySelector('button[aria-controls="faq4_desc"]');
+  const answer = testPageDocument.querySelector('#faq1_desc');
+  btn.setAttribute('aria-expanded', 'true');
+  answer.style.display = 'block';
+  // Hide the link after the button so it doesn't get in the way
+  testPageDocument.querySelector('#afterlink').style.display = 'none';
+  btn.focus();
+}
+
 export function moveFocusToFirstDisclosure(testPageDocument) {
   // sets focus on the first disclosure button
   testPageDocument.querySelector('button[aria-controls="faq1_desc"]').focus();
+}
+
+export function moveFocusToFourthDisclosure(testPageDocument) {
+  // sets focus on the fourth disclosure button
+  testPageDocument.querySelector('button[aria-controls="faq4_desc"]').focus();
 }

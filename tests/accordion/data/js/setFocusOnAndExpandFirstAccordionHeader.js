eb33fad28cc309831eb0a3b44fc227cdbe763b55
IsaDC <isa.delcastillo5@gmail.com>
2025-05-19 11:38:12 -0600

    Add setupScript files

--- tests/accordion/data/js/setFocusOnAndExpandFirstAccordionHeader.js
+++
@@ -1,3 +0,0 @@
-// sets focus on, and expands  the 'Personal Information' accordion header
-testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-testPageDocument.querySelector('button[aria-controls="sect1"]').focus();
--- tests/accordion/data/js/setFocusOnSecondAccordionHeaderAndHideFirstAccordionContent.js
+++ tests/accordion/data/js/setFocusOnSecondAccordionHeaderAndHideFirstAccordionContent.js
@@ -1,3 +1,17 @@
-// sets focus on the 'Billing Address' accordion header, and hides the content associated with the 'Personal Information' accordion header while expanding it
-testPageDocument.defaultView.disclosureController.toggleExpand(0, true);
-testPageDocument.querySelector('button[aria-controls="sect2"]').focus();
+// sets focus on the 'Billing Address' accordion header, and hides the content associated with the 'Personal Information' accordion header
+(() => {
+  const header1Button = document.querySelector('#accordion1id');
+  const panel1 = document.querySelector('#sect1');
+  const header2Button = document.querySelector('#accordion2id');
+
+  if (header1Button && panel1 && header2Button) {
+    header1Button.setAttribute('aria-expanded', 'false');
+    panel1.setAttribute('hidden', ''); // or panel1.hidden = true;
+
+    header2Button.focus();
+  } else {
+    console.error(
+      'SetupScript 3 Error: Accordion elements not found (#accordion1id, #sect1, or #accordion2id)'
+    );
+  }
+})();

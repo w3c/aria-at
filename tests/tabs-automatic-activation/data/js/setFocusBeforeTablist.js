98821c3467e93dd1bfabc56198e357bda631b86e
IsaDC <isa.delcastillo5@gmail.com>
2025-06-06 17:39:40 -0600

    Add setupScript to set focus before the tablist

--- tests/tabs-automatic-activation/data/js/setFocusBeforeTablist.js
+++ tests/tabs-automatic-activation/data/js/setFocusBeforeTablist.js
@@ -1,1 +1,7 @@
 // sets focus on a link before the tab list
+function setupScript() {
+  const linkBefore = document.getElementById('beforelink');
+  if (linkBefore) {
+    linkBefore.focus();
+  }
+}

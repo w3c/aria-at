da46474ddc1234ac1e24be7a0705568c8096a4f5
IsaDC <isa.delcastillo5@gmail.com>
2025-07-08 13:09:45 -0600

    Add new indentation to `setupScript`

--- tests/tabs-automatic-activation/data/js/deactivateFirstTabAndSetFocusBeforeTablist.js
+++ tests/tabs-automatic-activation/data/js/deactivateFirstTabAndSetFocusBeforeTablist.js
@@ -5,13 +5,13 @@ const firstPanel = document.querySelector('#tabpanel-1');
 
 // Deactivate the tab and hide the panel if they exist
 if (firstTab && firstPanel) {
-firstTab.setAttribute('aria-selected', 'false');
-firstTab.setAttribute('tabindex', '-1');
-firstPanel.classList.add('is-hidden');
+  firstTab.setAttribute('aria-selected', 'false');
+  firstTab.setAttribute('tabindex', '-1');
+  firstPanel.classList.add('is-hidden');
 }
 
 // Find the link before the example and set focus on it
 const linkBefore = document.querySelector('#beforelink');
 if (linkBefore) {
-linkBefore.focus();
+  linkBefore.focus();
 }

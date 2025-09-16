d2c5a752cfe1f6f8e703335930febb41ccdbb837
IsaDC <isa.delcastillo5@gmail.com>
2025-09-16 09:52:09 -0600

    Correct `setupScripts` to set `switch` to `checked`

--- tests/switch-checkbox/data/js/setFocusBeforeAndTurnOnSwitch.js
+++ tests/switch-checkbox/data/js/setFocusBeforeAndTurnOnSwitch.js
@@ -1,3 +1,3 @@
 // sets focus on a link before the switch, and sets the state of the switch to 'on'
-testPageDocument.querySelector('[role="switch"]').setAttribute('aria-checked', 'true');
+testPageDocument.querySelector('[role="switch"]').setAttribute('checked', 'true');
 testPageDocument.getElementById('beforelink').focus();

d5c0e07abdce1eee9940e0cfd50d5af13779e1b5
IsaDC <isa.delcastillo5@gmail.com>
2025-10-01 15:26:40 -0600

    remove switch-checkbox unfinished test plan

--- tests/switch-checkbox/data/js/setFocusAfterSwitch.js
+++
@@ -1,2 +0,0 @@
-// sets focus on a link after the switch
-testPageDocument.getElementById('afterlink').focus();
--- tests/switch-checkbox/data/js/setFocusOnAndTurnOnSwitch.js
+++
@@ -1,3 +0,0 @@
-// sets focus on the 'Notifications' switch, and sets its state to 'on'
-testPageDocument.querySelector('[role="switch"]').setAttribute('checked', 'true');
-testPageDocument.querySelector('[role="switch"]').focus();

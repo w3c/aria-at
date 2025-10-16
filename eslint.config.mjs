7e50bc6987ddbaa8550ce3e0e5d27ffa213bea2f
Howard Edwards <howarde.edwards@gmail.com>
2025-10-16 13:52:36 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig([
     'build/tests/**/*',
     'build/resources/**/*',
     // keys are manually parsed in process-test-directory.js and depend on format
-    'tests/resources/keys.mjs',
+    'resources/keys.mjs',
 
     // Automated tests related resources
     '__test__/__mocks__/tests/**/reference/**/*',

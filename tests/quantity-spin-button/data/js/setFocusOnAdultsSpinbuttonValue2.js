30e85e68e6d18272343bf76af6cb7efd66a040fc
IsaDC <isa.delcastillo5@gmail.com>
2025-10-23 20:16:42 -0600

    Add trimmed example and test plan csv files

---
+++ tests/quantity-spin-button/data/js/setFocusOnAdultsSpinbuttonValue2.js
@@ -0,0 +1,5 @@
+// "sets focus on the 'Adults' spinbutton and sets value to 2"
+'use strict';
+var el = document.getElementById('adults');
+el.value = '2';
+el.focus();
---
+++ tests/quantity-spin-button/data/js/setFocusOnAdultsSpinbuttonValue9.js
@@ -0,0 +1,5 @@
+// "sets focus on the 'Adults' spinbutton and sets value to 9"
+'use strict';
+var el = document.getElementById('adults');
+el.value = '9';
+el.focus();

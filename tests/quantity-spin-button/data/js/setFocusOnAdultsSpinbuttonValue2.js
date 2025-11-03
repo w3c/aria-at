0a2ac67153e30bf33a18e6906b7b3ca2d561d4e4
James Scholes <james@jamesscholes.com>
2025-11-03 13:39:37 -0600

    Update setup scripts again

--- tests/quantity-spin-button/data/js/setFocusOnAdultsSpinbuttonValue2.js
+++ tests/quantity-spin-button/data/js/setFocusOnAdultsSpinbuttonValue2.js
@@ -1,3 +1,3 @@
 // sets focus on the 'Adults' spinbutton and sets value to 2
-testPageDocument.defaultView.spinButtonController.setValue('2');
+testPageDocument.defaultView.spinButtonController.setValue('2', true);
 testPageDocument.getElementById('adults').focus();

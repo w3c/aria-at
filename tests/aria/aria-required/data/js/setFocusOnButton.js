1308d3086896746dd0aa8745c4e328a6a1463628
Matt King <a11yThinker@gmail.com>
2025-11-05 15:18:43 -0800

    Make test case and setup script

--- tests/aria/aria-required/data/js/setFocusOnButton.js
+++
@@ -1,2 +0,0 @@
-// sets focus on the 'Trigger Alert' button
-testPageDocument.querySelector('#alert-trigger').focus();
---
+++ tests/aria/aria-required/data/js/setRequiredFalse.js
@@ -0,0 +1,5 @@
+// sets the word is required checkbox to false
+const checkbox = testPageDocument.getElementById('toggle-required');
+if (checkbox) {
+  checkbox.checked = false;
+}

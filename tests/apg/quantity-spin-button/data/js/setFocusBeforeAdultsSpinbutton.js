f613c10d31b8acd814f0925ec15ee7f7831780a0
IsaDC <isa.delcastillo5@gmail.com>
2025-11-16 14:50:52 -0600

    Create directory for the Quantity spin button test plan

---
+++ tests/apg/quantity-spin-button/data/js/setFocusBeforeAdultsSpinbutton.js
@@ -0,0 +1,2 @@
+// sets focus on a link before the chespin button
+testPageDocument.getElementById('beforelink').focus();
---
+++ tests/apg/quantity-spin-button/data/js/setFocusOnAdultsSpinbuttonValue9.js
@@ -0,0 +1,3 @@
+// sets focus on the 'Adults' spinbutton and sets value to 9
+testPageDocument.defaultView.spinButtonController.setValue('9', true);
+testPageDocument.getElementById('adults').focus();
